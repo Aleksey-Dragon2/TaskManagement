7ac31b334f4a1b0b9368d5c8124a64ba3a9df787
Aleksey <aleha2552654@gmail.com>
2025-10-17 00:42:25 +0300

    Add TaskService

--- TaskManagement.Application/Abstractions/ITaskService.cs
+++ TaskManagement.Application/Abstractions/ITaskService.cs
@@ -2,5 +2,8 @@ namespace TaskManagement.Application.Abstractions;
 
 public interface ITaskService
 {
-    
+    Task<IEnumerable<TaskManagement.Domain.Entities.Task> > GetAllAsync();
+    Task CreateAsync(TaskManagement.Domain.Entities.Task task);
+    Task<bool> UpdateAsync(int id, bool isCompleted);
+    Task<bool> DeleteAsync(int id);
 }
--- TaskManagement.Application/Services/TaskService.cs
+++ TaskManagement.Application/Services/TaskService.cs
@@ -1,5 +1,36 @@
-﻿namespace TaskManagement.Application;
+﻿using TaskManagement.Application.Abstractions;
+using TaskManagement.Domain.Abstractions;
 
-public class TaskService
+namespace TaskManagement.Application.Services;
+
+public class TaskService : ITaskService
 {
+    private readonly ITaskRepository  _taskRepository;
+
+    public TaskService(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+    public async Task<IEnumerable<TaskManagement.Domain.Entities.Task>> GetAllAsync()
+    {
+        var tasks = await _taskRepository.GetAllAsync();
+        return tasks;
+    }
+
+    public async Task CreateAsync(TaskManagement.Domain.Entities.Task task)
+    {
+        await _taskRepository.CreateAsync(task);
+    }
+
+    public async Task<bool> UpdateAsync(int id,  bool isCompleted) 
+    {
+       var result = await  _taskRepository.UpdateAsync(id, isCompleted);
+        return result; 
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var result = await  _taskRepository.DeleteAsync(id);
+        return result;
+    }
 }
