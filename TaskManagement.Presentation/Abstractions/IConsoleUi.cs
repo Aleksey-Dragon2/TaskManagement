6a4da974101c07a4258f89b84bd79d96e520b8ab
Dragon <aleha2552654@gmail.com>
2025-10-20 14:35:53 +0300

    feat: added logic to ConsoleUI

---
+++ TaskManagement.Presentation/Abstractions/IConsoleUi.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement.Abstractions
+{
+    public interface IConsoleUi
+    {
+        public Task RunAsync();
+        public Task ShowTasksAsync();
+        public Task CreateTaskAsync();
+        public Task UpdateTaskAsync();
+        public Task DeleteTaskAsync();
+    }
+}
---
+++ TaskManagement.Presentation/Implementation/ConsoleUi.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using TaskManagement.Abstractions;
+using TaskManagement.Application.Abstractions;
+using TaskManagement.Application.Services;
+
+namespace TaskManagement.Implementation
+{
+    class ConsoleUi : IConsoleUi
+    {
+        private readonly ITaskService _taskService;
+
+        public ConsoleUi(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        public async Task RunAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("1. Show all tasks");
+                    Console.WriteLine("2. Create a new task");
+                    Console.WriteLine("3. Update status a task");
+                    Console.WriteLine("4. Delete a task");
+                    Console.WriteLine("0. Exit ");
+
+                    var choise = Console.ReadLine();
+                    switch (choise)
+                    {
+                        case "1":
+                            await ShowTasksAsync(); break;
+                        case "2":
+                            await CreateTaskAsync(); break;
+                        case "3":
+                            await UpdateTaskAsync(); break;
+                        case "4": 
+                            await DeleteTaskAsync(); break;
+                        case "0":
+                            return;
+                        default:
+                            Console.WriteLine("Invalid operation");
+                            break;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                } 
+            }
+        }
+
+        public async Task ShowTasksAsync()
+        {
+            var tasks = await _taskService.GetAllAsync();
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Id: {task.Id}\nTitle: {task.Title}\nDescription: {task.Description}" +
+                    $"\nCompleted: {task.IsComplete}\nDate of creation: {task.CreatedAt}");
+            }
+        }
+
+        public async Task CreateTaskAsync()
+        {
+            string title;
+            while (true)
+            {
+                Console.WriteLine("Enter task title:");
+                title = Console.ReadLine()?.Trim() ?? "";
+                if (!string.IsNullOrWhiteSpace(title))
+                    break;
+                Console.WriteLine("Please try again. Title cannot be empty");
+            }
+
+            string description;
+            while (true)
+            {
+                Console.Write("Enter task description:");
+                description = Console.ReadLine()?.Trim() ?? "";
+                if (!string.IsNullOrWhiteSpace(description))
+                    break;
+                Console.WriteLine("Please try again. Description cannot be empty");
+            }
+
+            var task = new TaskManagement.Domain.Entities.Task(title, description);
+            await _taskService.CreateAsync(task);
+            Console.WriteLine("Task Added");
+        }
+
+        public async Task UpdateTaskAsync()
+        {
+            int id;
+            while (true)
+            {
+                Console.Write("Enter task id:");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                Console.WriteLine("Id not found. Please try again");
+            }
+
+            var result = await _taskService.UpdateAsync(id);
+            if (result)
+                Console.WriteLine("Task has been updated");
+            else
+                Console.WriteLine("Task has not been updated");
+        }
+
+        public async Task DeleteTaskAsync()
+        {
+            while (true)
+            {
+                Console.Write("Enter task id:");
+                if (int.TryParse(Console.ReadLine(), out var id))
+                {
+                    await _taskService.DeleteAsync(id);
+                    return;
+                }
+                Console.WriteLine("Task not found. Please try again.");
+            }
+        }
+    }
+}
