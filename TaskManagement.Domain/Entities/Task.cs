c0fff60a380cd58ce76caae0b0d03c6ff19510a8
Dragon <aleha2552654@gmail.com>
2025-10-20 22:06:16 +0300

    refactor: variable name

--- TaskManagement.Domain/Entities/Task.cs
+++ TaskManagement.Domain/Entities/Task.cs
@@ -5,7 +5,7 @@ public class Task
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public bool IsComplete { get; set; }
+    public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
 
     public Task(string title, string description)
@@ -14,10 +14,15 @@ public class Task
             throw new ArgumentException("Title cannot be null or empty");
         if (string.IsNullOrWhiteSpace(description))
             throw new ArgumentException("Description cannot be null or empty");
-        
+
         Title = title;
         Description = description;
-        IsComplete = false;
+        IsCompleted = false;
         CreatedAt = DateTime.UtcNow;
+    }
+
+    public Task()
+    {
+
     }
 }
